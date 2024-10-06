2721ee098e65660f58991a6dd75a28501fc7cd0f
Phyu <115089908+phyuphyu285@users.noreply.github.com>
2024-10-06 19:40:25 +0630

    Add files via upload

---
+++ script_login.js
@@ -0,0 +1,24 @@
+const logForm = document.getElementById("loginForm");
+logForm.addEventListener("submit", myFunction);
+function myFunction(event){  
+ // document
+ // .getElementById("loginForm")
+//.addEventListener("submit", function (event) {
+   event.preventDefault();
+   const username = document.getElementById("lusername").value;
+   const password = document.getElementById("lpassword").value;
+
+   const user = localStorage.getItem(username);
+
+   if (user) {
+     const parsedUser = JSON.parse(user);
+     if (parsedUser.password === password) {
+       //localStorage.setItem("user", JSON.stringify(parsedUser));
+       window.location.href = "index.html";
+     } else {
+       alert("Incorrect password");
+     }
+   } else {
+     alert("User not found");
+   }
+ };
---
+++ script_register.js
@@ -0,0 +1,29 @@
+const regForm = document.getElementById("registerForm");
+regForm.addEventListener("submit", myFunction);
+function myFunction(event){     
+//document
+  //.getElementById("registerForm")
+ // .addEventListener("submit", function (event) {
+
+    event.preventDefault(); //to cancel submitting the form;
+    const name = document.getElementById("name").value;
+    const username = document.getElementById("rusername").value;
+    const password = document.getElementById("rpassword").value;
+    const confirmPassword =
+      document.getElementById("confirmPassword").value;
+
+    if (password != confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    const user = {
+      name: name,
+      username: username,
+      password: password,
+    };
+
+    localStorage.setItem(username, JSON.stringify(user));
+    alert("Registration successful! Please login.");
+    window.location.href = "index.html";
+  };
